6845cdf84c85b9c81bc3fc474fb544984d54dd80
Madhur-Sharma-64 <madhur.sharma.0064@gmail.com>
2026-08-03 10:44:16 +0530

    Add: last-stone-weight - submission-3

---
+++ Data Structures & Algorithms/last-stone-weight/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap;
+        for ( int stone : stones ) maxHeap.push(stone);
+
+        while ( maxHeap.size() > 1) {
+            int x = maxHeap.top();
+            maxHeap.pop();
+            int y = maxHeap.top();
+            maxHeap.pop();
+
+            maxHeap.push(x-y);
+        } 
+
+        return maxHeap.top();
+    }
+};
